aef589725e5980b690bfa5576d0c7de983446f60
hoanggggf <106527331+vuhoang001@users.noreply.github.com>
2024-03-10 22:23:07 +0700

    Cài đặt pug

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log('ok')
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express()
+const port: number = 3000
+
+app.set('views', "./views")
+app.set("view engine", "pug")
+
+app.get('/topics', (req: Request, res: Response) => {
+    res.render('client/pages/topics/index')
+})
+
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`)
+})
