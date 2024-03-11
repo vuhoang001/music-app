cf7eacd3321b0e6f5dff2084f3fbd128ed869145
hoanggggf <106527331+vuhoang001@users.noreply.github.com>
2024-03-11 11:25:58 +0700

    Làm trang chủ bài hát (client side)

--- controllers/client/topics.controller.ts
+++ controllers/client/topics.controller.ts
@@ -9,7 +9,8 @@ export const topics = async (req: Request, res: Response) => {
         deleted: false
     })
     res.render('client/pages/topics/index', {
-        pageTitle: "Chủ đề bài hát"
+        pageTitle: "Chủ đề bài hát",
+        topics: topics
     })
 }
 
