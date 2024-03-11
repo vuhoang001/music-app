d1cf753caf3791ea65a3c1e5cdf19df41e654616
hoanggggf <106527331+vuhoang001@users.noreply.github.com>
2024-03-11 10:28:04 +0700

    Controller

---
+++ controllers/client/topics.controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+
+import Topic from "../../models/topic.model";
+
+
+// [GET] /topics/
+export const topics = async (req: Request, res: Response) => {
+    const topics = await Topic.find({
+        deleted: false
+    })
+    res.render('client/pages/topics/index',)
+}
+
+
--- routes/client/topic.route.ts
+++ routes/client/topic.route.ts
@@ -1,15 +1,10 @@
 import { Request, Response, Router } from "express";
+import * as controller from "../../controllers/client/topics.controller"
 const router: Router = Router()
 import Topic from "../../models/topic.model";
 
 
 
-router.get('/', async (req: Request, res: Response)=> {
-    const topics = await Topic.find({
-        deleted: false
-    })
-    console.log(topics)
-    res.render('client/pages/topics/index')
-})
+router.get('/', controller.topics)
 
 export const topicRouter: Router = router
